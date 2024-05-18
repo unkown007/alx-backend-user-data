e36e09d4a840ab57b26e0f6f875cca7eb9e3e534
unkown007 <victormatusse00@gmail.com>
2024-05-18 22:46:08 +0200

    added file

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module defines encriptions functions to password """
+import bcrypt
+
+
+def hash_password(pwd: str) -> bytes:
+    """ Return a salted, hashed password """
+    pwd_bytes = pwd.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt)
