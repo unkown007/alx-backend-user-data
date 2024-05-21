8a67d21693dab452d8e8225fe2b725b8520bfe7e
unkown007 <victormatusse00@gmail.com>
2024-05-21 18:32:14 +0200

    updates

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,11 @@ def unauthorized() -> str:
     """ GET /api/v1//unauthorized
     """
     abort(401)
+
+
+@app_views.route("/forbidden", strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbiden
+    Raises an 403 error
+    """
+    abort(403)
