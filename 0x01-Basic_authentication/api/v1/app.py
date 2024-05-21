72d9af66880e76eef3dbe0ff170ff57379a9719c
unkown007 <victormatusse00@gmail.com>
2024-05-21 22:31:18 +0200

    added file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,6 +6,8 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 import os
 
 
@@ -13,6 +15,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_cls = {
+    "auth": Auth,
+    "basic_auth": BasicAuth
+}
 
 
 @app.errorhandler(404)
@@ -60,8 +66,7 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
 
-    if getenv("AUTH_TYPE", "") == "auth":
-        from api.v1.auth.auth import Auth
-        auth = Auth()
+    if getenv("AUTH_TYPE") is not None and getenv("AUTH_TYPE") in auth_cls:
+        auth = auth_cls.get(getenv("AUTH_TYPE"))()
 
     app.run(host=host, port=port)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module to implement Basic API authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class
+    """
+    pass
