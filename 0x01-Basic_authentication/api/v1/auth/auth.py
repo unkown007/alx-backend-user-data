5b7ebcec382a832593e528f758062075e2a4a776
unkown007 <victormatusse00@gmail.com>
2024-05-25 19:25:32 +0200

    updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,8 +24,15 @@ class Auth:
             return True
         if len(excluded_paths) == 0:
             return True
-        for pth in excluded_paths:
-            if re.search(f"^{path}", pth) is not None:
+        for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+            pattern = ''
+            if exclusion_path[-1] == '*':
+                pattern = '{}.*'.format(exclusion_path[0:-1])
+            elif exclusion_path[-1] == '/':
+                pattern = '{}/*'.format(exclusion_path[0:-1])
+            else:
+                pattern = '{}/*'.format(exclusion_path)
+            if re.match(pattern, path):
                 return False
         return True
 
