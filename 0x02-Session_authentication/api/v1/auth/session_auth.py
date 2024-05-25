6d0c02db702ac34ead23cc6ffb8f0528e015dc6d
unkown007 <victormatusse00@gmail.com>
2024-05-25 22:56:36 +0200

    updates

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,7 +30,5 @@ class SessionAuth(Auth):
         Returns:
             user id
         """
-        if not isinstance(session_id, str):
-            return None
-
-        return self.user_id_by_session_id.get(session_id)
+        if isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
