66d10a043d78ccbec6cbee9cec197d2715b1ded2
unkown007 <victormatusse00@gmail.com>
2024-05-26 15:54:43 +0200

    updates

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,7 +62,7 @@ def authenticate():
     path = request.path
     if auth is not None and auth.require_auth(path, excluded_paths):
         if auth.authorization_header(request) is None and\
-                auth.session_cookie(request):
+                auth.session_cookie(request) is None:
             abort(401)
         request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
